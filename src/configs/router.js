78895bff8e0a0ff01ff1b0087d380694baa071c0
cdev <cuong170603@gmail.com>
2023-10-12 22:22:34 +0700

    Initialzilation vacciantion-campaign

---
+++ src/configs/router.js
@@ -0,0 +1,9 @@
+const routers = {
+    home: '/',
+    campaignAgenda: '/campaign/:id',
+    sessionDetail: '/campaign/:id/:session',
+    login: '/login',
+    signup: '/signup',
+}
+
+export default routers;
